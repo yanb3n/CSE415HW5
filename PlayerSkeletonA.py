337d47362cad4a2022e89d7e5206da733bf6d7e8
SpartanBlue <jgao117@uw.edu>
2019-05-08 15:28:01 -0700

    More stuff

--- PlayerSkeletonA.py
+++ PlayerSkeletonA.py
@@ -10,12 +10,13 @@ global output
 def parameterized_minimax(currentState, alphaBeta=False, ply=3, useBasicStaticEval=True, useZobristHashing=False):
     '''Implement this testing function for your agent's basic
     capabilities here.'''
+    board_list = BC.parse(currentState.board)  # list of current board positions in row-major order
     if useBasicStaticEval:
         output['CURRENT_STATE_STATIC_EVAL'] = basicStaticEval(currentState)  # implement minimax algorithm here
     elif alphaBeta:
-        pass
+        pass  # temporary
     elif useZobristHashing:
-        pass
+        pass  # temporary
     output['N_STATES_EXPANDED'] = 0  # get states from minimax algorithm
     output['N_STATIC_EVALS'] = 0
     output['N_CUTOFFS'] = 0
@@ -50,24 +51,23 @@ def introduce():
     return '''I'm Gary Exasparov, a \"champion\" Baroque Chess agent.
     I was created by Jeffrey Gao (jgao117) and Ben Yan (yanb3).'''
 
+# initialize data structures and fields here
 def prepare(player2Nickname):
     ''' Here the game master will give your agent the nickname of
     the opponent agent, in case your agent can use it in some of
     the dialog responses.  Other than that, this function can be
     used for initializing data structures, if needed.'''
-    # initialize data structures and fields here
-    # let agent know if player is white or black
     output = {'CURRENT_STATE_STATIC_EVAL': None, 'N_STATES_EXPANDED': 0, 'N_STATIC_EVALS': 0, 'N_CUTOFFS': 0}
     pass
 
+# Implement minimax algorithm here
 def basicStaticEval(state):
     '''Use the simple method for state evaluation described in the spec.
     This is typically used in parameterized_minimax calls to verify
     that minimax and alpha-beta pruning work correctly.'''
-    # implement minimax algorithm here
-    list_of_pieces = state.parse()
     pass
 
+# Implement alpha-beta pruning here
 def staticEval(state):
     '''Compute a more thorough static evaluation of the given state.
     This is intended for normal competitive play.  How you design this
